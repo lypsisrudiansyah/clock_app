083366e121d6ae9f9a355363016996915e4740e5
rudiansyah <rudiansyahether@gmail.com>
2020-10-12 13:36:10 +0700

    feat: add flutter local notification
    feat: add configuration Provider and Flutter Local Notification on main dart
    
    feat: add assets of flutter local notification
    
    refactor(manifest): configure local notification AndroidManifest main and App Delegate Swift(iOS)

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,37 @@
+import 'package:clock_app/constants/constants.dart';
+import 'package:clock_app/model/menu_info_model.dart';
 import 'package:clock_app/pages/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:provider/provider.dart';
 
-void main() {
+// import 'constants/constants.dart';
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  var initializationSettingsAndroid = AndroidInitializationSettings('learn_more');
+      
+  var initializationSettingsIOS = IOSInitializationSettings(
+    requestAlertPermission: true,
+    requestBadgePermission: true,
+    requestSoundPermission: true,
+    onDidReceiveLocalNotification: (int id, String title, String body, String payload) async {}
+  );
+
+  var initializationSettings = InitializationSettings(
+    android: initializationSettingsAndroid,
+    iOS: initializationSettingsIOS,
+  );
+
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings, onSelectNotification: (String payload) async {
+    if (payload != null) {
+      debugPrint("Notification Payload : " + payload);
+    }
+  });
   runApp(MyApp());
 }
 
@@ -16,6 +46,9 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.blue,
           visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
-        home: HomePage());
+        home: ChangeNotifierProvider<MenuInfo>(
+          create: (context) => MenuInfo(MenuType.clock),
+          child: HomePage(),
+        ));
   }
 }
